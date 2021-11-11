523205e1034e9274131d9ed039200ac9cd6fdcc4
MiguelAngelMirandaBaena <the.mikimouseo@gmail.com>
2021-11-11 15:27:03 -0400

    Interfaces

---
+++ src/pojos/Cpu.java
@@ -0,0 +1,41 @@
+package pojos;
+
+import interfaces.IReseteable;
+
+import java.util.Date;
+
+public class Cpu extends Tecnologico implements IReseteable {
+    private int memoriaPrincipal;
+
+    public Cpu(String codigo, String modelo, String paisOrigen, Date fechaFabricacion, Empresa fabricante, int memoriaPrincipal){
+        super(codigo, modelo, paisOrigen, fechaFabricacion, fabricante);
+
+        this.setMemoriaPrincipal(memoriaPrincipal);
+    }
+
+
+    public int getMemoriaPrincipal() {
+        return memoriaPrincipal;
+    }
+
+    public void setMemoriaPrincipal(int memoriaPrincipal) {
+        this.memoriaPrincipal = memoriaPrincipal;
+    }
+
+    @Override
+    public String toString() {
+        return "Cpu{" +
+                "memoriaPrincipal=" + memoriaPrincipal +
+                ", codigo='" + codigo + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", paisOrigen='" + paisOrigen + '\'' +
+                ", fechaFabricacion=" + fechaFabricacion +
+                ", fabricante=" + fabricante +
+                '}';
+    }
+
+    @Override
+    public void reset() {
+        System.out.println("Reseteando al estado de fabrica...");
+    }
+}
